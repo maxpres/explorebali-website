36e696b0d6b624f4a2d6b7b9d642e42adccdc1ab
maxpres <maximilian.pres@web.de>
2022-03-19 18:27:35 +0100

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let timeline = new TimelineMax();
 
 // === OVERLAY=== //
 
-TweenMax.to(".bg", 3.5, {
+TweenMax.to('.bg', 3.5, {
   delay: 2.75,
   // top: "0%",
   opacity: 1,
@@ -13,8 +13,8 @@ TweenMax.to(".bg", 3.5, {
 
 // === SVG ANIMATION DASH BERECHNUNG == //
 
-const logo = document.querySelectorAll("#logo path");
-const logo2 = document.querySelectorAll("#logo2 path");
+const logo = document.querySelectorAll('#logo path');
+const logo2 = document.querySelectorAll('#logo2 path');
 
 for (let i = 0; i < logo.length; i++) {
   // console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
@@ -23,7 +23,7 @@ for (let i = 0; i < logo.length; i++) {
 // === ELEMTE FLIEGEN EIN === //
 
 TweenMax.staggerTo(
-  "nav ul li",
+  'nav ul li',
   0.7,
   {
     delay: 4.75,
@@ -35,7 +35,7 @@ TweenMax.staggerTo(
 );
 
 TweenMax.staggerTo(
-  ".social-media i",
+  '.social-media i',
   1,
   {
     delay: 5.75,
@@ -46,96 +46,96 @@ TweenMax.staggerTo(
   0.27
 );
 
-TweenMax.to(".bars", 1, {
+TweenMax.to('.bars', 1, {
   delay: 4.75,
   opacity: 1,
   y: 5,
   ease: Expo.easeInOut,
 });
 
-TweenMax.to(".scrolldown", 2, {
+TweenMax.to('.scrolldown', 2, {
   delay: 6,
   opacity: 0,
   y: -50,
   opacity: 1,
-  margin: "auto",
+  margin: 'auto',
   ease: Expo.easeInOut,
 });
 
 let tl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".second-title",
-    start: "-100%",
-    end: "20%",
+    trigger: '.second-title',
+    start: '-100%',
+    end: '20%',
     scrub: 1.5,
   },
 });
 
-tl.fromTo(".social-media", { right: "5%" }, { right: "5%", opacity: 0 });
+tl.fromTo('.social-media', { right: '5%' }, { right: '5%', opacity: 0 });
 
 let tl6 = gsap.timeline({
   scrollTrigger: {
-    trigger: ".second-title",
-    start: "-100%",
-    end: "20%",
+    trigger: '.second-title',
+    start: '-100%',
+    end: '20%',
     scrub: 1.5,
   },
 });
 
-tl6.fromTo(".scrolldown", { bottom: "5%" }, { bottom: "5%", opacity: 0 });
+tl6.fromTo('.scrolldown', { bottom: '5%' }, { bottom: '5%', opacity: 0 });
 
 let tl3 = gsap.timeline({
   scrollTrigger: {
-    trigger: "nav", //Startpunkt
-    start: "top",
-    end: "20%",
+    trigger: 'nav', //Startpunkt
+    start: 'top',
+    end: '20%',
     scrub: 1.5, //Geschwindigkeit der Elemente
   },
 });
 
-tl3.fromTo(".main-title", { x: "0" }, { x: "150", skewX: "10deg", opacity: 0 });
+tl3.fromTo('.main-title', { x: '0' }, { x: '150', skewX: '10deg', opacity: 0 });
 
 let tl4 = gsap.timeline({
   scrollTrigger: {
-    trigger: "nav", //Startpunkt
-    start: "top",
-    end: "20%",
+    trigger: 'nav', //Startpunkt
+    start: 'top',
+    end: '20%',
     scrub: 1.5, //Geschwindigkeit der Elemente
   },
 });
 
 tl4.fromTo(
-  ".second-title",
-  { x: "0" },
-  { x: "-150", skewX: "-10deg", opacity: 0 }
+  '.second-title',
+  { x: '0' },
+  { x: '-150', skewX: '-10deg', opacity: 0 }
 );
 
-const bars = document.querySelector(".bars");
-const navLinks = document.querySelectorAll(".ulhide li");
-const ulhide = document.querySelector(".ulhide");
+const bars = document.querySelector('.bars');
+const navLinks = document.querySelectorAll('.ulhide li');
+const ulhide = document.querySelector('.ulhide');
 
 const navSlide = () => {
-  ulhide.classList.toggle("nav-active");
-  bars.classList.toggle("toggle");
+  ulhide.classList.toggle('nav-active');
+  bars.classList.toggle('toggle');
 };
 
 bars.onclick = function () {
-  bars.classList.toggle("active");
+  bars.classList.toggle('active');
   navSlide();
 };
 
 document.onclick = function (e) {
-  if (e.target.id !== "barsid" && e.target.id !== "ullist") {
-    ulhide.classList.remove("nav-active");
-    bars.classList.remove("active");
+  if (e.target.id !== 'barsid' && e.target.id !== 'ullist') {
+    ulhide.classList.remove('nav-active');
+    bars.classList.remove('active');
   }
 };
 
-const swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
   loop: true,
   coverflowEffect: {
     rotate: 20,
@@ -145,32 +145,31 @@ const swiper = new Swiper(".swiper-container", {
     slideShadows: true,
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
   },
 });
 
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 
-window.addEventListener("scroll", () => {
-  let current = "";
+window.addEventListener('scroll', () => {
+  let current = '';
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
     if (pageYOffset > sectionTop - sectionHeight / 16) {
-      current = section.getAttribute("id");
+      current = section.getAttribute('id');
     }
   });
-  // console.log(current);
   navLinks.forEach((link) => {
-    link.classList.remove("active");
+    link.classList.remove('active');
     if (link.classList.contains(current)) {
-      link.classList.add("active");
+      link.classList.add('active');
     }
   });
 });
 
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 //=====COUNTER=====///
@@ -181,7 +180,7 @@ const observer = new IntersectionObserver(onIntersection, {
 });
 let oberserverindex = 0;
 
-observer.observe(document.querySelector(".info-section"));
+observer.observe(document.querySelector('.info-section'));
 
 function onIntersection(entries) {
   const [entry] = entries;
@@ -190,12 +189,12 @@ function onIntersection(entries) {
   if (entry.isIntersecting) {
     oberserverindex++;
     console.log(oberserverindex);
-    const counters = document.querySelectorAll(".counter");
+    const counters = document.querySelectorAll('.counter');
     counters.forEach((counter) => {
-      counter.innerText = "0";
+      counter.innerText = '0';
 
       const updateCounter = () => {
-        const target = +counter.getAttribute("data-target");
+        const target = +counter.getAttribute('data-target');
 
         const c = +counter.innerText;
 
@@ -217,7 +216,7 @@ function onIntersection(entries) {
 
 //===========================================================//
 const navHeight = nav.getBoundingClientRect().height;
-const scroll = document.querySelector(".scrollTop");
+const scroll = document.querySelector('.scrollTop');
 
 // console.log(navHeight); //55.927
 const navobserver = new IntersectionObserver(onNavIntersection, {
@@ -226,31 +225,31 @@ const navobserver = new IntersectionObserver(onNavIntersection, {
   rootMargin: `-${navHeight}px`,
 });
 
-navobserver.observe(document.querySelector(".container"));
+navobserver.observe(document.querySelector('.container'));
 
 function onNavIntersection(entries) {
   const [entry] = entries;
   // console.log(entry);
 
   if (!entry.isIntersecting) {
-    nav.classList.add("sticky");
-    scroll.classList.add("active");
+    nav.classList.add('sticky');
+    scroll.classList.add('active');
   } else {
-    nav.classList.remove("sticky");
-    scroll.classList.remove("active");
+    nav.classList.remove('sticky');
+    scroll.classList.remove('active');
   }
 }
 
 //===============Reveal Section============================//
 
-const allSections = document.querySelectorAll(".section");
+const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
 
   if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section-hidden");
+  entry.target.classList.remove('section-hidden');
   observer.unobserve(entry.target);
 };
 
@@ -261,18 +260,18 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add("section-hidden");
+  section.classList.add('section-hidden');
 });
 
 //=========SCROLL TO SECTION====
 
-const navlinks = document.querySelectorAll(".navlink");
+const navlinks = document.querySelectorAll('.navlink');
 
 navlinks.forEach(function (navlink) {
-  navlink.addEventListener("click", function (e) {
+  navlink.addEventListener('click', function (e) {
     e.preventDefault();
-    const id = this.getAttribute("href");
+    const id = this.getAttribute('href');
     console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   });
 });
